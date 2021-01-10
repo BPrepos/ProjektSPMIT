df0516ed8076fa1e78bc607607da48910ea3bd2c
Konrad Misterski <kmisterski@o2.pl>
2021-01-10 15:13:06 +0100

    return positions from algorithm

--- src/sample/Algorithm/AlgorithmWrapper.java
+++ src/sample/Algorithm/AlgorithmWrapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class AlgorithmWrapper {
 
-    public int[] calculate(ObservableList<Medicine> medicines, int startPositionX, int startPositionY) {
+    public List<Position> calculate(ObservableList<Medicine> medicines, int startPositionX, int startPositionY) {
         List<Position> positions = new ArrayList<>();
 
         positions.add(new Position(startPositionX, startPositionY));
@@ -17,7 +17,14 @@ public class AlgorithmWrapper {
             positions.add(new Position(medicine.getPosX(), medicine.getPosY()));
         }
 
-        return AlgorithmTSP.runAlgorithm(positions);
+        int[] order = AlgorithmTSP.runAlgorithm(positions);
+        List<Position> sortedPositions = new ArrayList<>();
+
+        for (int position : order) {
+            sortedPositions.add(positions.get(position));
+        }
+
+        return sortedPositions;
     }
 
 }
